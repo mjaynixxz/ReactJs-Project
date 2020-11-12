624a7c616fe75212fbd2c3a76f3a2a430efb357a
jaynixxz <41976904+mjaynixxz@users.noreply.github.com>
2020-11-12 10:27:02 +0100

    Youtube search App
    YouTube search using the youtubeAPI, Developed using both Hooks and Clss based components

---
+++ youtubevidapp/src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+class SearhBar extends React.Component {
+    
+    state = { term: ''};
+
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value });
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
+    };
+
+    render() {
+        return (
+            <div className="ui segment searchbar">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label>Video Search</label>
+                            <input 
+                            type="text" value={this.state.term} onChange={this.onInputChange}
+                             />
+                        </div>
+                </form>
+
+            </div>
+
+        );
+    }
+}
+
+export default SearhBar;
